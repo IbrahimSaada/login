95a7ac3609d4d77f751816bd4c66711d3be6baae
Ahmad Ghosen <ahmadghosen20@gmail.com>
2024-07-11 19:55:29 +0300

    edit verefication page new page and edit on the old one

--- lib/login/verification_page.dart
+++ lib/login/verification_page.dart
@@ -1,6 +1,5 @@
-import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
-import 'login.dart';
+
 
 void main() => runApp(MaterialApp(
       debugShowCheckedModeBanner: false,
@@ -8,7 +7,7 @@ void main() => runApp(MaterialApp(
     ));
 
 class VerificationPage extends StatelessWidget {
-  final int codeLength = 6; // Define the code length here
+  final int codeLength = 6; // Set the length of the verification code
 
   @override
   Widget build(BuildContext context) {
@@ -32,17 +31,19 @@ class VerificationPage extends StatelessWidget {
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
-                  FadeInUp(
-                      duration: Duration(milliseconds: 1000),
-                      child: Text(
-                        "Verify Your Account",
-                        style: TextStyle(color: Colors.white, fontSize: 40),
-                      )),
-                 
+                  Text(
+                    "Verify Your Account",
+                    style: TextStyle(color: Colors.white, fontSize: 40),
+                  ),
+                  SizedBox(height: 10),
+                  Text(
+                    "Please enter the verification code sent to your email",
+                    style: TextStyle(color: Colors.white, fontSize: 18),
+                  ),
                 ],
               ),
             ),
-           SizedBox(height: 20),
+            SizedBox(height: 20),
             Expanded(
               child: Container(
                 decoration: BoxDecoration(
@@ -68,7 +69,7 @@ class VerificationPage extends StatelessWidget {
                               borderRadius: BorderRadius.circular(10),
                               boxShadow: [
                                 BoxShadow(
-                                    color: Color.fromRGBO(225, 95, 27,.3),
+                                    color: Color.fromRGBO(225, 95, 27, .3),
                                     blurRadius: 20,
                                     offset: Offset(0, 10))
                               ],
@@ -93,40 +94,38 @@ class VerificationPage extends StatelessWidget {
                           );
                         }),
                       ),
-                           SizedBox(
-                        height: 40,
+                      SizedBox(height: 20),
+                      MaterialButton(
+                        
+                        height: 50,
+                        color: Colors.orange[900],
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(50),
+                        ),
+                        onPressed: () {  },
+                        child: Center(
+                          child: Text(
+                            "Verify",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 18,
+                            ),
+                          ),
+                        ),
                       ),
-                     FadeInUp(
-  duration: Duration(milliseconds: 1600),
-  child: MaterialButton(
-    onPressed: () {
-      Navigator.of(context).push(
-        MaterialPageRoute(builder: (context) => LoginPage()),
-      );
-    },
-    height: 50,
-    color: Colors.orange[900],
-    shape: RoundedRectangleBorder(
-      borderRadius: BorderRadius.circular(50),
-    ),
-    child: Center(
-      child: Text(
-        "Verify",
-        style: TextStyle(
-          color: Colors.white,
-          fontWeight: FontWeight.bold,
-        ),
-      ),
-    ),
-  ),
-)
-                  ],
+                      SizedBox(height: 10),
+                      Text(
+                        "By tapping Verify, you agree to our Terms.",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(color: Colors.grey.shade700),
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
-            ),
           ],
-          
         ),
       ),
     );

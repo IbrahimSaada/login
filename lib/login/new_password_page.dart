7ea3f2dfa987202b30523c55262336285393eed0
battomi <nenebattomi@gmail.com>
2024-07-21 18:45:31 +0300

    implemented new password functionality

--- lib/login/new_password_page.dart
+++ lib/login/new_password_page.dart
@@ -1,9 +1,44 @@
 import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
+import '../services/PasswordResetService.dart';
 import 'login_page.dart';
 
-class NewpasswordPage extends StatelessWidget {
-  const NewpasswordPage({super.key});
+class NewpasswordPage extends StatefulWidget {
+  final String emailOrPhone;
+  final String verificationCode;
+
+  const NewpasswordPage(
+      {super.key, required this.emailOrPhone, required this.verificationCode});
+
+  @override
+  _NewpasswordPageState createState() => _NewpasswordPageState();
+}
+
+class _NewpasswordPageState extends State<NewpasswordPage> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _confirmPasswordController =
+      TextEditingController();
+
+  Future<void> _resetPassword() async {
+    if (_formKey.currentState!.validate()) {
+      String newPassword = _passwordController.text.trim();
+      try {
+        await PasswordResetService().resetPassword(
+            widget.emailOrPhone, widget.verificationCode, newPassword);
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text('Password has been reset successfully')),
+        );
+        Navigator.of(context).push(
+          MaterialPageRoute(builder: (context) => const LoginPage()),
+        );
+      } catch (e) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Error: $e')),
+        );
+      }
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,28 +46,31 @@ class NewpasswordPage extends StatelessWidget {
       body: Container(
         width: double.infinity,
         decoration: BoxDecoration(
-            gradient: LinearGradient(begin: Alignment.topCenter, colors: [
-          Colors.orange.shade900,
-          Colors.orange.shade800,
-          Colors.orange.shade400
-        ])),
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            colors: [
+              Colors.orange.shade900,
+              Colors.orange.shade800,
+              Colors.orange.shade400,
+            ],
+          ),
+        ),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-            const SizedBox(
-              height: 80,
-            ),
+            const SizedBox(height: 80),
             Padding(
               padding: const EdgeInsets.all(20),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
                   FadeInUp(
-                      duration: const Duration(milliseconds: 1000),
-                      child: const Text(
-                        "Change Paasword",
-                        style: TextStyle(color: Colors.white, fontSize: 40),
-                      )),
+                    duration: const Duration(milliseconds: 1000),
+                    child: const Text(
+                      "Change Password",
+                      style: TextStyle(color: Colors.white, fontSize: 40),
+                    ),
+                  ),
                 ],
               ),
             ),
@@ -40,113 +78,123 @@ class NewpasswordPage extends StatelessWidget {
             Expanded(
               child: Container(
                 decoration: const BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(60),
-                        topRight: Radius.circular(60))),
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(60),
+                    topRight: Radius.circular(60),
+                  ),
+                ),
                 child: Padding(
                   padding: const EdgeInsets.all(30),
                   child: SingleChildScrollView(
-                    // Add this
-                    child: Column(
-                      children: <Widget>[
-                        const SizedBox(
-                          height: 60,
-                        ),
-                        FadeInUp(
+                    child: Form(
+                      key: _formKey,
+                      child: Column(
+                        children: <Widget>[
+                          const SizedBox(height: 60),
+                          FadeInUp(
                             duration: const Duration(milliseconds: 1400),
                             child: Container(
                               decoration: BoxDecoration(
-                                  color: Colors.white,
-                                  borderRadius: BorderRadius.circular(10),
-                                  boxShadow: const [
-                                    BoxShadow(
-                                        color: Color.fromRGBO(225, 95, 27, .3),
-                                        blurRadius: 20,
-                                        offset: Offset(0, 10))
-                                  ]),
+                                color: Colors.white,
+                                borderRadius: BorderRadius.circular(10),
+                                boxShadow: const [
+                                  BoxShadow(
+                                    color: Color.fromRGBO(225, 95, 27, .3),
+                                    blurRadius: 20,
+                                    offset: Offset(0, 10),
+                                  ),
+                                ],
+                              ),
                               child: Column(
                                 children: <Widget>[
                                   Container(
                                     padding: const EdgeInsets.all(10),
                                     decoration: BoxDecoration(
-                                        border: Border(
-                                            bottom: BorderSide(
-                                                color: Colors.grey.shade200))),
-                                    child: const TextField(
-                                      decoration: InputDecoration(
-                                          hintText: "Enter New Password",
-                                          hintStyle:
-                                              TextStyle(color: Colors.grey),
-                                          border: InputBorder.none),
+                                      border: Border(
+                                        bottom: BorderSide(
+                                          color: Colors.grey.shade200,
+                                        ),
+                                      ),
+                                    ),
+                                    child: TextFormField(
+                                      controller: _passwordController,
+                                      decoration: const InputDecoration(
+                                        hintText: "Enter New Password",
+                                        hintStyle:
+                                            TextStyle(color: Colors.grey),
+                                        border: InputBorder.none,
+                                      ),
+                                      obscureText: true,
+                                      validator: (value) {
+                                        if (value == null || value.isEmpty) {
+                                          return 'Please enter your new password';
+                                        }
+                                        if (!RegExp(
+                                                r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$')
+                                            .hasMatch(value)) {
+                                          return 'Password must be at least 8 characters long and include at least one uppercase letter, one lowercase letter, one number, and one special character';
+                                        }
+                                        return null;
+                                      },
                                     ),
                                   ),
-                                ],
-                              ),
-                            )),
-                        const SizedBox(
-                          height: 60,
-                        ),
-                        FadeInUp(
-                            duration: const Duration(milliseconds: 1400),
-                            child: Container(
-                              decoration: BoxDecoration(
-                                  color: Colors.white,
-                                  borderRadius: BorderRadius.circular(10),
-                                  boxShadow: const [
-                                    BoxShadow(
-                                        color: Color.fromRGBO(225, 95, 27, .3),
-                                        blurRadius: 20,
-                                        offset: Offset(0, 10))
-                                  ]),
-                              child: Column(
-                                children: <Widget>[
                                   Container(
                                     padding: const EdgeInsets.all(10),
                                     decoration: BoxDecoration(
-                                        border: Border(
-                                            bottom: BorderSide(
-                                                color: Colors.grey.shade200))),
-                                    child: const TextField(
-                                      decoration: InputDecoration(
-                                          hintText: "Confirm New Password",
-                                          hintStyle:
-                                              TextStyle(color: Colors.grey),
-                                          border: InputBorder.none),
+                                      border: Border(
+                                        bottom: BorderSide(
+                                          color: Colors.grey.shade200,
+                                        ),
+                                      ),
+                                    ),
+                                    child: TextFormField(
+                                      controller: _confirmPasswordController,
+                                      decoration: const InputDecoration(
+                                        hintText: "Confirm New Password",
+                                        hintStyle:
+                                            TextStyle(color: Colors.grey),
+                                        border: InputBorder.none,
+                                      ),
+                                      obscureText: true,
+                                      validator: (value) {
+                                        if (value == null || value.isEmpty) {
+                                          return 'Please confirm your new password';
+                                        }
+                                        if (value != _passwordController.text) {
+                                          return 'Passwords do not match';
+                                        }
+                                        return null;
+                                      },
                                     ),
                                   ),
                                 ],
                               ),
-                            )),
-                        const SizedBox(
-                          height: 40,
-                        ),
-                        FadeInUp(
-                          duration: const Duration(milliseconds: 1600),
-                          child: MaterialButton(
-                            onPressed: () {
-                              Navigator.of(context).push(
-                                MaterialPageRoute(
-                                    builder: (context) => const LoginPage()),
-                              );
-                            },
-                            height: 50,
-                            color: Colors.orange[900],
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(50),
                             ),
-                            child: const Center(
-                              child: Text(
-                                "Change",
-                                style: TextStyle(
-                                  color: Colors.white,
-                                  fontWeight: FontWeight.bold,
+                          ),
+                          const SizedBox(height: 40),
+                          FadeInUp(
+                            duration: const Duration(milliseconds: 1600),
+                            child: MaterialButton(
+                              onPressed: _resetPassword,
+                              height: 50,
+                              color: Colors.orange[900],
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(50),
+                              ),
+                              child: const Center(
+                                child: Text(
+                                  "Change",
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold,
+                                  ),
                                 ),
                               ),
                             ),
                           ),
-                        )
-                      ],
+                        ],
+                      ),
                     ),
                   ),
                 ),

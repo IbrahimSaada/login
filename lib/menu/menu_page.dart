9f8c784b64afce5c71fef8fae69cdbb8893ec14e
Ahmad Ghosen <ahmadghosen20@gmail.com>
2024-07-23 22:27:38 +0300

    add setting page

--- lib/menu/menu_page.dart
+++ lib/menu/menu_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:login2/menu/profile_page.dart';
 import 'package:login2/login/login_page.dart';
+import 'package:login2/menu/setting_page.dart';
+
 class MenuPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -11,56 +13,63 @@ class MenuPage extends StatelessWidget {
       body: SingleChildScrollView(
         child: Column(
           children: [
-   Padding(
-  padding: const EdgeInsets.all(16.0),
-  child: GestureDetector(
-    onTap: () {
-      Navigator.push(
-        context,
-        MaterialPageRoute(builder: (context) => ProfilePage()),
-      );
-    },
-    child: Row(
-      children: [
-        CircleAvatar(
-          backgroundImage: AssetImage('assets/chef.jpg'),
-          radius: 30,
-        ),
-        SizedBox(width: 16),
-        Text(
-          'Ahmad Ghosen',
-          style: TextStyle(
-            fontSize: 20,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-      ],
-    ),
-  ),
-),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => ProfilePage()),
+                  );
+                },
+                child: Row(
+                  children: [
+                    CircleAvatar(
+                      backgroundImage: AssetImage('assets/chef.jpg'),
+                      radius: 30,
+                    ),
+                    SizedBox(width: 16),
+                    Text(
+                      'Ahmad Ghosen',
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
             ListTile(
               leading: Icon(Icons.people, color: Colors.lightBlue),
               title: Text('Friends'),
             ),
-             ListTile(
+            ListTile(
               leading: Icon(Icons.bookmark, color: Colors.lightBlueAccent),
               title: Text('Saved Post'),
             ),
             ListTile(
               leading: Icon(Icons.settings, color: Colors.orange),
               title: Text('Setting'),
+               onTap: () {
+                // logout function
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(builder: (context) => SettingPage()),
+                );
+              },
             ),
-   ListTile(
-  leading: Icon(Icons.logout, color: Colors.pink),
-  title: Text('Logout'),
-  onTap: () {
-    // logout function
-    Navigator.pushReplacement(
-      context, 
-      MaterialPageRoute(builder: (context) => LoginPage()),
-    );
-  },
-),
+            ListTile(
+              leading: Icon(Icons.logout, color: Colors.pink),
+              title: Text('Logout'),
+              onTap: () {
+                // logout function
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(builder: (context) => LoginPage()),
+                );
+              },
+            ),
           ],
         ),
       ),
--- lib/menu/setting_page.dart
+++ lib/menu/setting_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'package:login2/login/login_page.dart';
+
 class SettingPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
